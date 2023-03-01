c7877c7ca1c9ad83325ef9478ac194dd2c7d7cd8
Zura Sekhniashvili <sekhniashvilizura@gmail.com>
2023-03-02 01:22:29 +0400

    Generate UserResource

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationItem;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Filament::serving(function () {
+            Filament::registerNavigationItems([
+                NavigationItem::make('thecodeholic.com')
+                    ->url('https://thecodeholic.com', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-globe-alt')
+                    ->group('Content Creator')
+                    ->sort(3),
+            ]);
+        });
     }
 }
