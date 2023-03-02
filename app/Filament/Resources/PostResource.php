4b6bc9165342bd9ebb3cec352c3b10f03dfba05d
Zura Sekhniashvili <sekhniashvilizura@gmail.com>
2023-03-02 09:48:25 +0400

    Add delete action to PostResource table actions

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -75,6 +75,7 @@ class PostResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
