b595a84a4564e05f0007cd742837fda6435f251a
Zura Sekhniashvili <sekhniashvilizura@gmail.com>
2023-03-02 09:47:36 +0400

    Rename migration for category_post

--- database/migrations/2023_03_01_202715_create_category_post_table.php
+++ database/migrations/2023_03_01_202715_create_category_post_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('category_post', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->references('id')->on('categories');
-            $table->foreignId('post_id')->references('id')->on('posts');
+            $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreignId('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->timestamps();
         });
     }
