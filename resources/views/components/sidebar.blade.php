0529396a1a8bc7112ef7ce0b63390d984f830cae
Zura Sekhniashvili <sekhniashvilizura@gmail.com>
2023-03-04 00:59:50 +0400

    Create sidebar component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,14 @@
+<aside class="w-full md:w-1/3 flex flex-col items-center px-3">
+
+    <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+        <p class="text-xl font-semibold pb-5">
+            {{\App\Models\TextWidget::getTitle('about-us-sidebar')}}
+        </p>
+        {!! \App\Models\TextWidget::getContent('about-us-sidebar') !!}
+        <a href="#"
+           class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+            Get to know us
+        </a>
+    </div>
+
+</aside>
