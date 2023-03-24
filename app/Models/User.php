a9447842454c8bb051389a9c7a1ba14f910f5d69
Zura Sekhniashvili <sekhniashvilizura@gmail.com>
2023-03-25 00:17:02 +0400

    Install permissions package and add admin role

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail, FilamentUser
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +44,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        return $this->hasRole('admin');
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\Post::factory(50)->create();
+//         \App\Models\Post::factory(50)->create();
+
+        /** @var \App\Models\User $adminUser */
+        $adminUser = User::factory()->create([
+            'email' => 'admin@example.com',
+            'name' => 'Admin',
+            'password' => bcrypt('admin123')
+        ]);
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminUser->assignRole($adminRole);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
