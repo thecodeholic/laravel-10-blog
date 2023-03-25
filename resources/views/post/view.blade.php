cc909b693fa701f9707a2e1c9d8b2d05c29c32c6
Zura Sekhniashvili <sekhniashvilizura@gmail.com>
2023-03-25 14:45:35 +0400

    Fix next link bug on post view page

--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -42,14 +42,17 @@
                 @endif
             </div>
             <div class="w-1/2">
-                <a href="{{route('view', $next)}}" class="block w-full bg-white shadow hover:shadow-md text-right p-6">
-                    <p class="text-lg text-blue-800 font-bold flex items-center justify-end">Next
-                        <i
-                            class="fas fa-arrow-right pl-1"></i></p>
-                    <p class="pt-2">
-                        {{\Illuminate\Support\Str::words($next->title, 5)}}
-                    </p>
-                </a>
+                @if($next)
+                    <a href="{{route('view', $next)}}"
+                       class="block w-full bg-white shadow hover:shadow-md text-right p-6">
+                        <p class="text-lg text-blue-800 font-bold flex items-center justify-end">Next
+                            <i
+                                class="fas fa-arrow-right pl-1"></i></p>
+                        <p class="pt-2">
+                            {{\Illuminate\Support\Str::words($next->title, 5)}}
+                        </p>
+                    </a>
+                @endif
             </div>
         </div>
 
