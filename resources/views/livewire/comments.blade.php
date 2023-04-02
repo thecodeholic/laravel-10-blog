195d75d2c3199e11b6867bf03da8c193423c4bc9
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-04-02 14:13:02 +0400

    Remove printing of comments.

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -2,7 +2,6 @@
     <livewire:comment-create :post="$post" />
 
     @foreach($comments as $comment)
-        <pre>{{$comment->comments}}</pre>
         <livewire:comment-item :comment="$comment" wire:key="comment-{{$comment->id}}-{{$comment->comments->count()}}" />
     @endforeach
 </div>
