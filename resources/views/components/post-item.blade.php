bf7014df87d109381c56922be51f9e6a86386de7
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-04-02 10:33:21 +0400

    Add aspect ratio on image
    Use category title on home page

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,7 +1,7 @@
 <article class="bg-white    flex flex-col shadow my-4">
     <!-- Article Image -->
     <a href="{{route('view', $post)}}" class="hover:opacity-75">
-        <img src="{{$post->getThumbnail()}}">
+        <img src="{{$post->getThumbnail()}}" alt="{{$post->title}}" class="aspect-[4/3] object-contain">
     </a>
     <div class="bg-white flex flex-col justify-start p-6">
         <div class="flex gap-4">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,22 +59,25 @@
         </div>
 
         <!-- Latest Categories -->
-        <div>
-            <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
-                Recent Categories
-            </h2>
 
-            @foreach($categories as $category)
+        @foreach($categories as $category)
+            <div>
+                <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
+                    Category "{{$category->title}}"
+                    <a href="{{route('by-category', $category)}}">
+                        <i class="fas fa-arrow-right"></i>
+                    </a>
+                </h2>
+
                 <div class="mb-6">
-                    <h3 class="text-xl font-bold text-center">{{$category->title}}</h3>
                     <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
                         @foreach($category->publishedPosts()->limit(3)->get() as $post)
-                            <x-post-item :post="$post" :show-author="false" />
+                            <x-post-item :post="$post" :show-author="false"/>
                         @endforeach
                     </div>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
 
     </div>
 </x-app-layout>
